98f4cfc093ac7ee390894732722201a102e0e1ff
kksdl <a1b2c3d4e6f5@sina.com>
2018-07-20 17:53:35 +0800

    system(pause)

--- example1/main.c
+++ example1/main.c
@@ -6,13 +6,14 @@ int main()
     printf("请输入两个实型数据：\n");
     scanf("%d%d", &a, &b);
     if(a > b){
-        printf("第一个数大，%d > %d", a, b);
+        printf("第一个数大，%d > %d\n", a, b);
     }else if(a < b){
-        printf("第二个数大，%d < %d", a, b);
+        printf("第二个数大，%d < %d\n", a, b);
     }else{
-        printf("两个数一样大，%d = %d", a, b);
+        printf("两个数一样大，%d = %d\n", a, b);
     }
     //c=a>b?b:a;
     //直接输出值
+    //system("pause");
     return 0;
 }
